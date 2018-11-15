d95cdcc46d49c9a7d53ae475421dfb5df7d4746a
JonasHans <jonas.vanoenen@gmail.com>
2018-11-15 16:11:43 +0100

    Query method for BRON

--- python/indexBRON.py
+++ python/indexBRON.py
@@ -1,54 +1,114 @@
 import pandas as pd
+from pandas import ExcelWriter
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+from utils.timeit import timeit
+
+# Unique identifier for one run which can be used to match results
+UNIQUE_IDENTIFIER = str(int(time.time()))
 
 def analyzeBRON(bron):
-    total = 0
-    filled = 0
-    percentages = []
+	global UNIQUE_IDENTIFIER
+	total = 0
+	filled = 0
+	percentages = []
 
-    for feature in bron:
-        # Get lengths
-        featureLen = len(bron[feature])
-        filledLen = len(bron[bron[feature].notnull()])
+	for feature in bron:
+		# Get lengths
+		featureLen = len(bron[feature])
+		filledLen = len(bron[bron[feature].notnull()])
 
-        # Add to counters
-        total += featureLen
-        filled += filledLen
+		# Add to counters
+		total += featureLen
+		filled += filledLen
 
-        percentages.append((filledLen/featureLen)*100)
+		percentages.append((filledLen/featureLen)*100)
 
-    # Create bar plot of all features
-    y_pos = np.arange(len(list(bron)))
-    plt.barh(y_pos, percentages, align='center', alpha=0.5)
-    plt.yticks(y_pos, list(bron))
-    plt.yticks(fontsize=5)
-    plt.savefig('../results/bronFeatureInfo.png')
+	# Create bar plot of all features
+	y_pos = np.arange(len(list(bron)))
+	plt.barh(y_pos, percentages, align='center', alpha=0.5)
+	plt.yticks(y_pos, list(bron))
+	plt.yticks(fontsize=5)
+	plt.savefig('../results/bronFeatureInfo-'+UNIQUE_IDENTIFIER+'.png')
 
-    # Create bar plot of non null features
-    nnPercentages = []
-    nnLabels = []
-    for i in range(0,len(percentages)):
-        if percentages[i] != 0.0:
-            nnPercentages.append(percentages[i])
-            nnLabels.append(list(bron)[i])
-    y_pos = np.arange(len(list(nnLabels)))
-    plt.clf()
-    plt.barh(y_pos, nnPercentages, align='center', alpha=0.5)
-    plt.yticks(y_pos, nnLabels)
-    plt.yticks(fontsize=6)
-    plt.savefig('../results/bronFilledFeatureInfo.png')
+	# Create bar plot of non null features
+	nnPercentages = []
+	nnLabels = []
+	for i in range(0,len(percentages)):
+		if percentages[i] != 0.0:
+			nnPercentages.append(percentages[i])
+			nnLabels.append(list(bron)[i])
+	y_pos = np.arange(len(list(nnLabels)))
+	plt.clf()
+	plt.barh(y_pos, nnPercentages, align='center', alpha=0.5)
+	plt.yticks(y_pos, nnLabels)
+	plt.yticks(fontsize=6)
+	plt.savefig('../results/bronFilledFeatureInfo-'+UNIQUE_IDENTIFIER+'.png')
 
-    print('Number of features: ',len(list(bron)))
-    print('Total number of fields: ',total)
-    print('Total filled in: ',filled)
-    print('Percentage: ', (filled/total)*100)
+	print('Number of features: ',len(list(bron)))
+	print('Total number of fields: ',total)
+	print('Total filled in: ',filled)
+	print('Percentage: ', (filled/total)*100)
 
+# shows information on result set
+def resultsInfo(fileName, results):
+	global UNIQUE_IDENTIFIER
+
+	# Print feature information
+	f = open('../results/'+fileName+'-'+UNIQUE_IDENTIFIER+'.txt', "w")
+	f.write("Total number of features: "+str(len(list(results))) + "\n")
+	f.write("Feature names: \n")
+	for i in range(0, len(list(results)), 5):
+		f.write(str(list(results)[i:i+5]) + '\n')
+
+	# Gather stats on results
+	stats = results.describe()
+
+	# Result set information
+	f.write('\n')
+	f.write('Total number of results: ' + str(len(results)) + '\n')
+	# f.write(stats)
+
+@timeit
+# Method which returns all results associated with the query
+def queryBRON(bron_ongevallen, query):
+	qString = ''
+
+	# Build query string
+	for term in query:
+		if qString == '':
+			qString = term + '==\"' + query[term] + '\"'
+		else:
+			qString += ' & '+ term + '==\"' + query[term] + '\"'
+
+	# Execute query on the set
+	return bron_ongevallen.query(qString)
+
+@timeit
+def indexBRON():
+	# Read in the BRON ongevallen.txt
+	# TODO: Define dtypes
+	# dropna => axis=1 means we look at the columns, how=all means we only drop the column if all values are NaN
+	return [
+		pd.read_csv('../data/BRON2017/gegevens/Ongevallengegevens/ongevallen.txt', sep=',', index_col='VKL_NUMMER', low_memory=False).dropna(axis=1, how='all')
+		]
+
+@timeit
 def main():
-    # Read in the BRON ongevallen.txt
-    # TODO: Define dtypes
-    bron = pd.read_csv('../data/BRON2017/gegevens/Ongevallengegevens/ongevallen.txt', sep=',', index_col='VKL_NUMMER', low_memory=False)
-    analyzeBRON(bron)
+	# Index BRON dataset
+	[bron_ongevallen] = indexBRON()
+
+	# Analyse the BRON ongevallen dataset
+	# analyzeBRON(bron_ongevallen)
+
+	# Query the dataset
+	query = {
+		'AP3_CODE' : 'DOD',
+		'DAGTYPE' : 'MA-VR'
+	}
+	qResult = queryBRON(bron_ongevallen, query)
+	resultsInfo('queryBron', qResult)
 
 if __name__== "__main__":
-    main()
+	main()
